c17569fd9416305a62fd2841c7705bebd1e9a2fd
Maxwell Spadafore <maxspad@umich.edu>
2022-05-17 08:24:32 -0400

    still working on mbs

--- sched.py
+++ sched.py
@@ -14,7 +14,21 @@ RESIDENTS_CSV = 'residents.csv'
 MASTER_BLOCK_SCHEDULE_CSV = 'master_block_schedule.csv'
 OFF_SERVICE_HOURS_CSV = 'off_service_hours.csv'
 
-@st.experimental_memo
+def load_mbs(mbs_csv_loc : str) -> pd.DataFrame:
+    mbs = pd.read_csv(mbs_csv_loc, header=[0,1,2,3], index_col=0)
+    mbs = mbs.T # rows are dates, columns residents
+    mbs = mbs.reset_index()
+    mbs.index = pd.PeriodIndex(mbs['week_start'], freq='7D') # TODO this creates an off by one bug at end of academic year
+    mbs = mbs.drop(['block','week','week_start','week_end'], axis=1)
+
+    # mbs['week_start'] = mbs['week_start'].apply(pd.Timestamp)
+    # mbs['week_end'] = mbs['week_end'].apply(pd.Timestamp)
+    # mbs = mbs.set_index('week_start')
+
+    # mbs = mbs.set_index(['week_start', 'week_end', 'week', 'block'], drop=True)
+    return mbs
+
+# @st.experimental_memo
 def load_and_parse():
     # Download ShiftAdmin schedule
     s = parse_sched.download_ical(CALENDAR_URL)
@@ -26,13 +40,28 @@ def load_and_parse():
     # Read in resident list
     resdf = pd.read_csv(RESIDENTS_CSV)
     # Read in block schedule
-    mbs = pd.read_csv(MASTER_BLOCK_SCHEDULE_CSV, header=[0,1,2,3], index_col=0)
+    # mbs = pd.read_csv(MASTER_BLOCK_SCHEDULE_CSV, header=[0,1,2,3], index_col=0)
+    mbs = load_mbs(MASTER_BLOCK_SCHEDULE_CSV)
     # Read in off service hour listing
     osh = pd.read_csv(OFF_SERVICE_HOURS_CSV)
+    osh = osh.set_index('rotation')
     return sched, resdf, mbs, osh
 
 
 sched, resdf, mbs, osh = load_and_parse()
+print(mbs.index)
+mbs_starts = mbs.replace(osh['start'].to_dict())
+mbs_starts['col_type'] = 'start'
+mbs_ends = mbs.replace(osh['end'].to_dict())
+mbs_ends['col_type'] = 'end'
+mbs['col_type'] = 'shift'
+mbs = pd.concat([mbs_starts, mbs_ends, mbs])
+mbs = mbs.pivot(columns='col_type')
+mbs = mbs.resample('D').ffill()
+mbs = mbs.sort_index()
+# st.write(osh['start'].to_dict())
+print(mbs)
+# st.dataframe(mbs.apply(lambda x: x.apply(lambda yosh.loc[x,'start'], axis=0))
 
 # Define UI
 with st.expander('Options:'):
@@ -48,22 +77,22 @@ with st.expander('Options:'):
         default=['M Spadafore','M Newton', 'M Basinger'])
 
 # Clean UI inputs
-# Add the timezone to the inputs
+# Make the inputs timezone-aware
 start_dt = pd.Timestamp(st_date)
 start_dt = start_dt.tz_localize(TZ)
 end_dt = pd.Timestamp(en_date)
 end_dt = end_dt.tz_localize(TZ)
 
-st.write(f'Shifts between {start_dt} {start_dt.tzname()} and {end_dt} {end_dt.tzname()}')
+st.write(f'Shifts between {start_dt} {start_dt.tzname()} and {end_dt} {end_dt.tzname()}') # TODO for debugging
 # filter the schedule to the requested dates
 fs = sched.copy()
 fs = fs[start_dt:end_dt]
 
 # filter the schedule to the requested residents
-st.write(f'For residents {" ".join(resident_choices)}')
+st.write(f'For residents {" ".join(resident_choices)}') # TODO for debugging
 fs = fs[fs['resident'].isin(resident_choices)]
 
-st.write('Filtered schedule:')
+st.write('Filtered schedule:') # TODO for debugging
 st.dataframe(fs)
 # add some extra columns to make things easier 
 fs['start_day'] = fs['start'].dt.dayofyear
@@ -71,8 +100,10 @@ fs['end_day'] = fs['end'].dt.dayofyear
 fs['start_hour'] = fs['start'].dt.hour
 fs['end_hour'] = fs['end'].dt.hour
 
-mbs.T
-osh
+# add in off service rotations
+# mbs = mbs.drop(['block','week','week_end'], axis=1)
+# mbs
+
 # Build a matrix where rows are residents and columns are hours
 # 0-hour/column is the first hour (midnight) of start_dt
 # the value of the matrix at res,hr is 1 if the resident is working
