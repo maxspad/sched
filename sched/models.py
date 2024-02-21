3e845406272fabc0063aa4d810e973ca7a716e85
Maxwell Spadafore <maxspad@umich.edu>
2024-02-21 09:20:36 -0500

    getting ready to import shifts

--- sched/models.py
+++ sched/models.py
@@ -1,66 +1,91 @@
-from pydantic import BaseModel, Field, computed_field, StrictInt
-from typing import List, Optional, Union, Literal
+from pydantic import BaseModel, Field, computed_field, StrictInt, EmailStr
+from typing import List, Optional, Union, Literal, Dict
 from datetime import datetime, date, time, timedelta
 from enum import Enum
+
+class DayOfWeek(Enum):
+    Mon = 'Mon'
+    Tue = 'Tue'
+    Wed = 'Wed'
+    Thu = 'Thu'
+    Fri = 'Fri'
+    Sat = 'Sat'
+    Sun = 'Sun'
+
+ALL_DAYS_OF_WEEK = list(DayOfWeek)
+
+class Rule(BaseModel):
+    name : str
+    description : str = ''
+
+class ShiftRule(Rule):
+    pass
+
+class UserRule(Rule):
+    pass
+
+class ShiftType(BaseModel):
+    name : str
+    # shortName : str
+    startTime : timedelta
+    duration: timedelta
+    postBuffer : timedelta = timedelta(seconds=0)
+
+    isClinical : bool = True
+    isCounted : bool = True
+    isNight : bool = False
+
+    shiftRules : Optional[List[ShiftRule]] = None
+
+class StaffingRule(ShiftRule):
+     minStaffing : StrictInt
+     maxStaffing : StrictInt
+
+class WeeklyStaffingRule(StaffingRule):
+    appliesToDays : List[DayOfWeek] = ALL_DAYS_OF_WEEK
+    minStaffing : Union[StrictInt, Dict[DayOfWeek, StrictInt]]
+    maxStaffing : Union[StrictInt, Dict[DayOfWeek, StrictInt]]
+
+class DayOfMonthStaffingRule(StaffingRule):
+    dayOfMonth : timedelta
+
+class CustomDateStaffingRule(StaffingRule):
+    '''Set specific staffing for a certain shift on a certain day'''
+    customDate: date
+
+class WorkloadRule(UserRule):
+    pass
+
+class RotationType(BaseModel):
+    name : str
+    abbrv : Optional[str] = None
+    preBufferHours : int = 0
+    postBufferHours : int = 0
+    schedulableShifts : Union[Optional[List[ShiftType]], Literal['all']] = 'all'
+
+class ScheduledRotation(BaseModel):
+    rotationType : RotationType
+    startDate : date
+    endDate : date
 
 class User(BaseModel):
     firstName : str = Field(min_length=1)
     lastName : str = Field(min_length=1)
+    email : EmailStr
 
     @computed_field
     @property
     def schedName(self) -> str:
         return self.firstName[0] + ' ' + self.lastName
-    
+
 class Shift(BaseModel):
-    name : str
-    # shortName : str
-    startTime : timedelta
-    lengthHours : StrictInt
+    shiftType : ShiftType
+    startDate : date
+    assignedUser : Optional[User] = None
 
-class Rotation(BaseModel):
-    name : str 
-    preBufferHours : int = 0
-    postBufferHours : int = 0
-    schedulableShifts : Union[Optional[List[Shift]], Literal['all']] = 'all'
+import pandas as pd
 
-class EDRotation(Rotation):
-    schedulableShifts : Literal['all'] = 'all'
-    preBufferHours : Literal[0] = 0
-    postBufferHours : Literal[0] = 0
-
-class OffServiceRotation(Rotation):
-    schedulableShifts : List[Shift] = []
-    preBufferHours : int = 12
-    postBufferHours : int = 24
-
-class Shifts(Enum):
-    JAM = Shift(name='J AM', startTime=timedelta(hours=7), lengthHours=12),
-    JPM = Shift(name='J PM', startTime=timedelta(hours=19), lengthHours=12),
-    UX = Shift(name='UX', startTime=timedelta(hours=7), lengthHours=8),
-    UG = Shift(name='UG', startTime=timedelta(hours=22), lengthHours=9)
-
-class JeopardyOnlyRotation(Rotation):
-    schedulableShifts : List[Shift] = [Shifts.JAM, Shifts.JPM]
-
-icu_rotations = {
-    r : OffServiceRotation(name=r)
-    for r in ['PICU','MICU','CCMU','CVCICU','CICU','TBICU','NICU']
-}
-us_rotations = {
-    r : JeopardyOnlyRotation(name=r)
-    for r in ['US1','US2']
-}
-ed_rotations = {
-    r : EDRotation(name=r)
-    for r in ['ED']
-}
-rotations = dict(**icu_rotations, **us_rotations, **ed_rotations)
-
-
-if __name__ == "__main__":
-    m = User(firstName='Max', lastName='Spadafore')
-    print(rotations)
-    # UG = Shift(name='UG', shortName='UG', startTime=timedelta(hours=22), endTime=timedelta(hours=22+9))
-    # ED = Rotation(name='ED')
-    # PICU = Rotation(name='PICU', preBufferHours=12, postBufferHours=24, schedulableShifts=[])
+if __name__ == '__main__':
+    shifts_csv = pd.read_csv('data/shifts.csv')
+    shifts = [ShiftType(**r.to_dict()) for _, r in shifts_csv.iterrows()]
+    print(shifts)
